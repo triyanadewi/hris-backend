4d9282f5a125584df26b5ef407814ceb277b9f19
triyanadewi <triyanadewifatmawati@gmail.com>
2025-06-17 21:24:31 +0700

    fix: add allowed origin for API in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','http://127.0.0.1:3000'],
+    'allowed_origins' => ['http://localhost:3000','http://127.0.0.1:3000', 'https://api.hristeam11.web.id'],
 
     'allowed_origins_patterns' => [],
 
